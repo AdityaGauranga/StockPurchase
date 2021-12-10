481d426abdc7680ef67867ecaf377d8c2bb2624c
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-10 10:05:32 +0530

    added back ticks in js

--- stonks.js
+++ stonks.js
@@ -51,17 +51,18 @@ function calculateProfitAndLoss(initial, quantity, current) {
         var loss = decimal((initial * quantity) - (current * quantity));
         var lossPercentage = decimal((loss / (current * quantity)) * 100);
 
-        outputBox.textContent = ("The invested amount is ${(initial * quantity)} and loss is ${loss} and the percent is ${lossPercentage}%")
+        outputBox.textContent = (`The invested amount is ${(initial * quantity)} and loss is ${loss} and the percent is ${lossPercentage}%`)
         outputBox.style.color = "purple"
     }
-} else if (current > initial) {
+ else if (current > initial) {
     var profit = decimal((current * quantity) - (initial * quantity));
     var profitPercentage = decimal((profit / (initial * quantity)) * 100);
 
-    outputBox.textContent = ("The invested amount is ${(initial * quantity)} profit is ${profit} and the percent is ${profitPercentage}%")
+    outputBox.textContent = (`The invested amount is ${(initial * quantity)} profit is ${profit} and the percent is ${profitPercentage}%`)
     outputBox.style.color = "green"
 } else {
 
     outputBox.textContent = (`No Pain No Gain and No Gain No Pain`);
     outputBox.style.color = "yellow"
+}
 }
